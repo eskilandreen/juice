941588a1940db8e3f6ea52517944814bbd62a92b
Eskil Andréen <eskil.andreen@gmail.com>
2012-05-12 10:05:51 +0200

    Added generate and get using an in memory database

--- serve.py
+++ serve.py
@@ -1,15 +1,49 @@
 import web
+import json
+import random
+import uuid
+import hashlib
 
 urls = (
-    '/(.*)', 'hello'
+    '/generate', 'generate',
+    '/get/(\w+)', 'get',
 )
 app = web.application(urls, globals())
 
-class hello:
-    def GET(self, name):
-        if not name:
-            name = 'World'
-        return 'Hello, ' + name + '!'
+lookup = {}
+
+def generate_random_ingredients():
+    all_ingr = [x.strip() for x in file('ingredients.juice')]
+    ret = {}
+    for k in range(10):
+        fruit = random.choice(all_ingr)
+        if fruit in ret:
+            ret[fruit] += 100
+        else:
+            ret[fruit] = 100
+    return sorted(ret.items())
+
+class generate:
+    def GET(self):
+        ingr = generate_random_ingredients()
+        id = hashlib.md5(repr(ingr)).hexdigest()
+        lookup[id] = ingr
+        return json.dumps({
+            'id': id,
+            'ingredients': dict(ingr)
+        }, indent=4)
+
+class get:
+    def GET(self, id):
+        try:
+            ingr = lookup[id]
+            return json.dumps({
+                'id': id,
+                'ingredients': dict(ingr)
+            }, indent=4)
+        except KeyError:
+            raise web.notfound()
+
 
 if __name__ == "__main__":
     app.run()
